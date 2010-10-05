1da90840ce31f25551d5c743b7dc332613fb6570
beepscore <support@beepscore.com>
2010-10-04 21:01:54 -0700

    Factor duplicate local variables to global variables.

--- AutomationTests/testConvertUI.js
+++ AutomationTests/testConvertUI.js
@@ -1,5 +1,48 @@
 #import "tuneup.js"
 
+/////////////////////////////////////////////////////////////
+// Global variables
+UIALogger.logMessage("initializing global variables");
+
+// UIAApplication class mainWindow() method
+var window = UIATarget.localTarget().frontMostApp().mainWindow();
+
+// UIALogger.logMessage("declare contentView");
+// ????: In IB, I cast UIView to UIControl.  Is that confusing UIAutomation?
+// contentView is not part of the element tree
+// contentView elementTree equals UIAElementNil
+// var contentView = target.elements()["contentView"];
+
+var titleLabel = window.staticTexts()["Temperature Converter"];
+
+// var temperatureIn =  window.textFields()["temperatureIn"];
+// only one UITextField, it must be textFields()[0]
+var temperatureIn =  window.textFields()[0];
+
+// var temperatureOut = window.staticTexts()["temperatureOut"];
+// var temperatureOut = window.staticTexts()[0];
+var temperatureOut = window.elements()[1];
+
+// var fromTemperatureUnitSegment = window.segmentedControls()["fromTemperatureUnitSegment"];
+var fromTemperatureUnitSegment = window.segmentedControls()[0];
+var toTemperatureUnitSegment = window.segmentedControls()[1];
+
+// var fromK = fromTemperatureUnitSegment.buttons()["°R"].logElement();
+// var fromK = fromTemperatureUnitSegment.buttons()[3].logElement;
+// var fromK = window.buttons()[1].logElement();
+// var fromK = fromTemperatureUnitSegment.buttons().firstWithName("°K");
+var fromC = fromTemperatureUnitSegment.elements()[2];
+var fromF = fromTemperatureUnitSegment.elements()[3];
+var fromK = fromTemperatureUnitSegment.elements()[1];
+var fromR = fromTemperatureUnitSegment.elements()[0];
+
+var toC = toTemperatureUnitSegment.elements()[3];
+var toF = toTemperatureUnitSegment.elements()[2];
+var toK = toTemperatureUnitSegment.elements()[1];
+var toR = toTemperatureUnitSegment.elements()[0];
+/////////////////////////////////////////////////////////////
+
+
 // method |test| is defined in test.js
 // target and application are defined in test.js
 
@@ -13,50 +56,9 @@ test("testShouldPass", function(target, application) {
 
 test("testFtoC", function(target, application) {
 
-     UIALogger.logMessage("target.logElementTree()");
-     target.logElementTree();
-     
-     // UIAApplication class mainWindow() method
-     var window = application.mainWindow();
-     
-     // UIALogger.logMessage("declare contentView");
-     // ????: In IB, I cast UIView to UIControl.  Is that confusing UIAutomation?
-     // contentView is not part of the element tree
-     // contentView elementTree equals UIAElementNil
-     // var contentView = target.elements()["contentView"];
-     
-     var titleLabel = window.staticTexts()["Temperature Converter"];
-
-     // var temperatureIn =  window.textFields()["temperatureIn"];
-     // only one UITextField, it must be textFields()[0]
-     var temperatureIn =  window.textFields()[0];
-
-     // var temperatureOut = window.staticTexts()["temperatureOut"];
-     // var temperatureOut = window.staticTexts()[0];
-     var temperatureOut = window.elements()[1];
-     
-
-     // var fromTemperatureUnitSegment = window.segmentedControls()["fromTemperatureUnitSegment"];
-     var fromTemperatureUnitSegment = window.segmentedControls()[0];
-     var toTemperatureUnitSegment = window.segmentedControls()[1];
-     
-     // var fromK = fromTemperatureUnitSegment.buttons()["°R"].logElement();
-     // var fromK = fromTemperatureUnitSegment.buttons()[3].logElement;
-     // var fromK = window.buttons()[1].logElement();
-     // var fromK = fromTemperatureUnitSegment.buttons().firstWithName("°K");
-     var fromC = fromTemperatureUnitSegment.elements()[2];
-     var fromF = fromTemperatureUnitSegment.elements()[3];
-     var fromK = fromTemperatureUnitSegment.elements()[1];
-     var fromR = fromTemperatureUnitSegment.elements()[0];
-     
-     var toC = toTemperatureUnitSegment.elements()[3];
-     var toF = toTemperatureUnitSegment.elements()[2];
-     var toK = toTemperatureUnitSegment.elements()[1];
-     var toR = toTemperatureUnitSegment.elements()[0];
-     
      fromF.tap();
      toC.tap();
-
+     
      UIALogger.logMessage("temperatureIn.tap()");
      temperatureIn.tap();
      UIALogger.logMessage("Keyboard is showing. Call target.logElementTree()");
@@ -80,28 +82,6 @@ test("testFtoC", function(target, application) {
 
 test("testKtoC", function(target, application) {
      
-     var window = application.mainWindow();     
-     var titleLabel = window.staticTexts()["Temperature Converter"];
-     var temperatureIn =  window.textFields()[0];     
-     var temperatureOut = window.elements()[1];
-     
-     // var fromTemperatureUnitSegment = window.segmentedControls()["fromTemperatureUnitSegment"];
-     var fromTemperatureUnitSegment = window.segmentedControls()[0];
-     var toTemperatureUnitSegment = window.segmentedControls()[1];
-     
-     // var fromK = fromTemperatureUnitSegment.buttons()["°R"].logElement();
-     // var fromK = fromTemperatureUnitSegment.buttons()[3].logElement;
-     // var fromK = window.buttons()[1].logElement();
-     var fromC = fromTemperatureUnitSegment.elements()[2];
-     var fromF = fromTemperatureUnitSegment.elements()[3];
-     var fromK = fromTemperatureUnitSegment.elements()[1];
-     var fromR = fromTemperatureUnitSegment.elements()[0];
-
-     var toC = toTemperatureUnitSegment.elements()[3];
-     var toF = toTemperatureUnitSegment.elements()[2];
-     var toK = toTemperatureUnitSegment.elements()[1];
-     var toR = toTemperatureUnitSegment.elements()[0];
-     
      fromK.tap();
      toC.tap();
      // for a label, use name not value
@@ -109,9 +89,9 @@ test("testKtoC", function(target, application) {
      temperatureIn.setValue("0");     
      // tap titleLabel to dismiss keyboard
      titleLabel.tap();
-
+     
      assertEquals("-273.15", temperatureOut.name());
-
+     
      });
 
 
@@ -125,6 +105,6 @@ test("testKtoC", function(target, application) {
  
  
  test("FifteenCToF", function(target, application) {
-  });
+ });
  */
 
