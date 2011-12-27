55336410cb27d1e6f4c2485eeb76728e612d4757
beepscore <support@beepscore.com>
2011-12-26 23:28:08 -0800

    Indent JavaScript using TextMate. TextMate formats .js better than Xcode.

--- UIAutomationTests/convert-ui-test.js
+++ UIAutomationTests/convert-ui-test.js
@@ -17,83 +17,83 @@ var toTemperatureField = window.textFields()[1];
 
 
 test("testShouldPass", function(target, application) {
-     assertEquals(3, 3);
-     
-     var a = "fred";
-     assertEquals("fred", a);
-     });
+    assertEquals(3, 3);
+
+    var a = "fred";
+    assertEquals("fred", a);
+});
 
 test("testFifyNineFtoC", function(target, application) {
-     
-     window.segmentedControls()["fromTemperatureUnitSegment"].buttons()["°F"].tap();
-     target.delay(1);
-     fromTemperatureField.tap();
-     target.delay(1);
-     
-     //application.keyboard().typeString("59");
-     //application.keyboard().elements()["done"].tap();
-     application.keyboard().typeString("59\n");
-     target.delay(2);
-     
-     window.segmentedControls()["toTemperatureUnitSegment"].buttons()["°C"].tap();
-     target.delay(1);
-     
-     assertEquals("59", fromTemperatureField.value());
-     UIALogger.logMessage("toTemperatureField.name() = " + toTemperatureField.name());
-     UIALogger.logMessage("toTemperatureField.value() = " + toTemperatureField.value());
 
-     assertEquals("15", toTemperatureField.value());
-     });
+    window.segmentedControls()["fromTemperatureUnitSegment"].buttons()["°F"].tap();
+    target.delay(1);
+    fromTemperatureField.tap();
+    target.delay(1);
+
+    //application.keyboard().typeString("59");
+    //application.keyboard().elements()["done"].tap();
+    application.keyboard().typeString("59\n");
+    target.delay(2);
+
+    window.segmentedControls()["toTemperatureUnitSegment"].buttons()["°C"].tap();
+    target.delay(1);
+
+    assertEquals("59", fromTemperatureField.value());
+    UIALogger.logMessage("toTemperatureField.name() = " + toTemperatureField.name());
+    UIALogger.logMessage("toTemperatureField.value() = " + toTemperatureField.value());
+
+    assertEquals("15", toTemperatureField.value());
+});
 
 
 test("testZeroKtoC", function(target, application) {
-     
-     window.segmentedControls()["fromTemperatureUnitSegment"].buttons()["°K"].tap();
-     target.delay(1);
-     fromTemperatureField.tap();
-     target.delay(1);
-     
-     application.keyboard().typeString("0\n");
-     target.delay(2);
-     
-     window.segmentedControls()["toTemperatureUnitSegment"].buttons()["°C"].tap();
-     target.delay(1);
-     
-     assertEquals("0", fromTemperatureField.value());
-     assertEquals("-273.15", toTemperatureField.value());     
-     });
+
+    window.segmentedControls()["fromTemperatureUnitSegment"].buttons()["°K"].tap();
+    target.delay(1);
+    fromTemperatureField.tap();
+    target.delay(1);
+
+    application.keyboard().typeString("0\n");
+    target.delay(2);
+
+    window.segmentedControls()["toTemperatureUnitSegment"].buttons()["°C"].tap();
+    target.delay(1);
+
+    assertEquals("0", fromTemperatureField.value());
+    assertEquals("-273.15", toTemperatureField.value());     
+});
 
 
 test("testZeroKToR", function(target, application) {
-     
-     window.segmentedControls()["fromTemperatureUnitSegment"].buttons()["°K"].tap();
-     target.delay(1);
-     fromTemperatureField.tap();
-     target.delay(1);
-     
-     application.keyboard().typeString("0\n");
-     target.delay(2);
-     
-     window.segmentedControls()["toTemperatureUnitSegment"].buttons()["°R"].tap();
-     target.delay(1);
-     
-     assertEquals("0", fromTemperatureField.value());
-     assertEquals("0", toTemperatureField.value());      
-     });
+
+    window.segmentedControls()["fromTemperatureUnitSegment"].buttons()["°K"].tap();
+    target.delay(1);
+    fromTemperatureField.tap();
+    target.delay(1);
+
+    application.keyboard().typeString("0\n");
+    target.delay(2);
+
+    window.segmentedControls()["toTemperatureUnitSegment"].buttons()["°R"].tap();
+    target.delay(1);
+
+    assertEquals("0", fromTemperatureField.value());
+    assertEquals("0", toTemperatureField.value());      
+});
 
 
 test("testFifteenCToF", function(target, application) {
-     window.segmentedControls()["fromTemperatureUnitSegment"].buttons()["°C"].tap();
-     target.delay(1);
-     fromTemperatureField.tap();
-     target.delay(1);
-     
-     application.keyboard().typeString("15\n");
-     target.delay(2);
-     
-     window.segmentedControls()["toTemperatureUnitSegment"].buttons()["°F"].tap();
-     target.delay(1);
-     
-     assertEquals("15", fromTemperatureField.value());
-     assertEquals("59", toTemperatureField.value());     
-     });
+    window.segmentedControls()["fromTemperatureUnitSegment"].buttons()["°C"].tap();
+    target.delay(1);
+    fromTemperatureField.tap();
+    target.delay(1);
+
+    application.keyboard().typeString("15\n");
+    target.delay(2);
+
+    window.segmentedControls()["toTemperatureUnitSegment"].buttons()["°F"].tap();
+    target.delay(1);
+
+    assertEquals("15", fromTemperatureField.value());
+    assertEquals("59", toTemperatureField.value());     
+});
