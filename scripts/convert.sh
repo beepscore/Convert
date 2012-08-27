99fedc034892c9f6926689f5d48d4492a1fb5cb8
beepscore <support@beepscore.com>
2012-08-26 22:32:32 -0700

    Add scritpts/convert.sh. In Xcode add it as a folder reference.

---
+++ scripts/convert.sh
@@ -0,0 +1,2 @@
+instruments -t "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate" "/Users/stevebaker/xcode-build/Convert-cirgdecbzxoiobfeqabnkpoqfxnn/Build/Products/Release-iphonesimulator/Convert.app" -e UIASCRIPT "${WORKSPACE}/UIAutomationTests/convert-ui-test.js" -e UIARESULTSPATH ${WORKSPACE}
+
