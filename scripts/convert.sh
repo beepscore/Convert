789fd5bc9ad6e899efbbcc30a7b174d0a503949a
beepscore <support@beepscore.com>
2012-08-27 23:14:16 -0700

    In convert.sh run Instruments on simulator, comment out running pre-installed Convert.app, instead try to run from Jenkins build directory.

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,7 +1,12 @@
-echo "ls -la ${WORKSPACE}"
-ls -la ${WORKSPACE}
-echo "ls -la ${WORKSPACE}/build/"
+echo "List files to show build/Convert.app is present."
 ls -la ${WORKSPACE}/build/
 
-instruments -t "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate" "/Users/stevebaker/xcode-build/Convert-cirgdecbzxoiobfeqabnkpoqfxnn/Build/Products/Release-iphonesimulator/Convert.app" -e UIASCRIPT "${WORKSPACE}/UIAutomationTests/convert-ui-test.js" -e UIARESULTSPATH ${WORKSPACE}
+#If Convert.app is pre-installed on simulator, Jenkins UIAutomation runs it.
+#instruments -t "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate" "/Users/stevebaker/xcode-build/Convert-cirgdecbzxoiobfeqabnkpoqfxnn/Build/Products/Release-iphonesimulator/Convert.app" -e UIASCRIPT "${WORKSPACE}/UIAutomationTests/convert-ui-test.js" -e UIARESULTSPATH ${WORKSPACE}
+#output:
+#2012-08-28 05:28:13 +0000 Pass: test Temperature Tidbit label
+#Instruments Trace Complete (Duration : 52.651230s; Output : /Users/stevebaker/.jenkins/jobs/Convert/workspace/instrumentscli2.trace)
+
+#Run UIAutomation from new build
+instruments -t "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate" "${WORKSPACE}/build/Convert.app" -e UIASCRIPT "${WORKSPACE}/UIAutomationTests/convert-ui-test.js" -e UIARESULTSPATH ${WORKSPACE} 
 
