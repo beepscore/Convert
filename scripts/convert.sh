01a0eb5e1a8e1d54ff656dd4fe7501697e520bda
beepscore <support@beepscore.com>
2012-08-27 21:55:38 -0700

    Revise echo.

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,6 +1,6 @@
-echo "workspace"
+echo "ls -la ${WORKSPACE}"
 ls -la ${WORKSPACE}
-echo "workspace/build/"
+echo "ls -la ${WORKSPACE}/build/"
 ls -la ${WORKSPACE}/build/
 
 instruments -t "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate" "/Users/stevebaker/xcode-build/Convert-cirgdecbzxoiobfeqabnkpoqfxnn/Build/Products/Release-iphonesimulator/Convert.app" -e UIASCRIPT "${WORKSPACE}/UIAutomationTests/convert-ui-test.js" -e UIARESULTSPATH ${WORKSPACE}
