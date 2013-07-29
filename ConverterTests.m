7bed4436af04f99dbfe07fcc4eb32361fda87f23
beepscore <support@beepscore.com>
2013-07-28 19:48:35 -0700

    Add ConverterTests.m. Currently it has no methods.

---
+++ ConverterTests.m
@@ -0,0 +1,18 @@
+//
+//  ConverterTests.m
+//  Convert
+//
+//  Created by Steve Baker on 7/28/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ConverterTests : SenTestCase
+
+@end
+
+
+@implementation ConverterTests
+
+@end
